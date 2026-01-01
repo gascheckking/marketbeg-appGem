037258f07d62ff700a09b04497a5b31332c5af85
gascheckking <warpaiactivity1@gmail.com>
2026-01-01 16:03:01 +0100

    Update CameraBatch.tsx

--- components/CameraBatch.tsx
+++ components/CameraBatch.tsx
@@ -3,7 +3,7 @@ import { LoadingAI } from "./LoadingAI";
 export default function CameraBatch() {
   return (
     <div className="card">
-      <p>Ta ett eller flera foton.</p>
+      <p>Ta ett eller flera foton på objektet.</p>
       <button className="secondary">Ladda upp bilder</button>
       <LoadingAI />
     </div>
