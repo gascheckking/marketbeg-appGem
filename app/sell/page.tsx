10d82530bfd410ab5d72aa111b28cb63c8859987
gascheckking <warpaiactivity1@gmail.com>
2026-01-01 15:22:41 +0100

    Create page.tsx

---
+++ app/sell/page.tsx
@@ -0,0 +1,10 @@
+import CameraBatch from "../../components/CameraBatch";
+
+export default function SellPage() {
+  return (
+    <main className="container">
+      <h2>Sälj</h2>
+      <CameraBatch />
+    </main>
+  );
+}
