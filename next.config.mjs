6c2de99f5d397df9a31fbeadc24e4083c4ee13cb
gascheckking <warpaiactivity1@gmail.com>
2026-01-07 23:26:18 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Detta tillåter appen att bygga även om det finns små 
-  // TypeScript-varningar som annars stoppar bygget på Vercel
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
 };
 
 export default nextConfig;
