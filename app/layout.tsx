a7e14f4aa6eb05ccdad8a411b3e97df1b1f983bb
gascheckking <warpaiactivity1@gmail.com>
2026-01-14 22:09:20 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 // // app/layout.tsx
 import './globals.css'
+import Navbar from '@/components/Navbar'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="sv">
       <body style={{ background: '#000' }}>
-        <main>{children}</main>
+        <div style={{ maxWidth: '450px', margin: '0 auto', minHeight: '100vh', borderLeft: '1px solid #111', borderRight: '1px solid #111' }}>
+          {children}
+          <Navbar />
+        </div>
       </body>
     </html>
   )
